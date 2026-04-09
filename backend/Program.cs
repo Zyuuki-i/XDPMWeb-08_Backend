da2927681565ecb25bba4c439aa1e27a7cd5c96d
Lê Văn Đạt <levandat10a14@gmail.com>
2026-04-09 23:58:24 +0700

    update-4/9-1158

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new()
+    {
+        Title = "My API",
+        Version = "v1"
+    });
+});
 
 builder.Services.AddCors(options =>
 {
@@ -19,18 +24,18 @@ var app = builder.Build();
 
 app.UseCors("AllowAll");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = "swagger";
+});
+
 app.MapControllers();
+
 app.MapGet("/", () => "API is running! Vd: /api/nguoidung");
 
 app.Run();
